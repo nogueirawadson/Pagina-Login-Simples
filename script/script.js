e64573b1160b4c88f5fbf4c9c823208cc05ba436
nogueirawadson <nogueirawadson@outlook.pt>
2024-10-16 22:30:48 -0300

    Ajustes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
 
-const btncreateAccount = document.querySelector(".btncreateaccount");
 const accountcreate = document.querySelector(".createaccount");
-const btnloginaccount = document.querySelector(".btnloginaccount")
 const account = document.querySelector(".account")
 
-const ocultar = function() {
-  accountcreate.style.display === "flex"? accountcreate.style.display = "none" : accountcreate.style.display = "flex";
-  account.style.display === "none"? account.style.display = "flex":
-  account.style.display = "none";
+const ocultar = function () {
+  accountcreate.style.display === "flex" ? accountcreate.style.display = "none" : accountcreate.style.display = "flex";
+  account.style.display === "none" ? account.style.display = "flex" :
+    account.style.display = "none";
 }
 
 
